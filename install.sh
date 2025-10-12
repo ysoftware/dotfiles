459bb627024676dbdf1e44f97946e94ed3135dc5
Yaroslav Erohin <ysoftware@yandex.ru>
2025-10-12 13:43:37 +0200

    update tmux conf

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ UP_TO_DATE_MSG="Up to date: "
 TMUX_CONF_IN="$DIR/tmux.conf"
 TMUX_CONF_OUT="$HOME/.tmux.conf"
 if ! grep -Fsq "source-file $TMUX_CONF_IN" "$TMUX_CONF_OUT"; then
-    echo "tmux source-file $TMUX_CONF_IN  # from: $DIR/install.sh" >> "$TMUX_CONF_OUT"
-    echo "'tmux source-file $TMUX_CONF_IN' -> $TMUX_CONF_OUT"
+    echo "source-file $TMUX_CONF_IN  # from: $DIR/install.sh" >> "$TMUX_CONF_OUT"
+    echo "'tmux: source-file $TMUX_CONF_IN' -> $TMUX_CONF_OUT"
 else
     echo "${UP_TO_DATE_MSG}${TMUX_CONF_OUT}"
 fi
@@ -17,7 +17,7 @@ NVIM_IN="$DIR/nvim.vim"
 NVIM_OUT="$HOME/.config/nvim/init.vim"
 if ! grep -Fsq "so $NVIM_IN" "$NVIM_OUT"; then
     echo "so $NVIM_IN \" from $DIR/install.sh" >> "$NVIM_OUT"
-    echo "'so $NVIM_IN' -> $NVIM_OUT"
+    echo "'nvim: so $NVIM_IN' -> $NVIM_OUT"
 else
     echo "${UP_TO_DATE_MSG}${NVIM_OUT}"
 fi
@@ -38,7 +38,7 @@ BASH_IN="$DIR/bashrc"
 BASHPROFILE_OUT="$HOME/.bash_profile"
 if ! grep -Fsq "source $BASH_IN" "$BASHPROFILE_OUT"; then
     echo "source $BASH_IN # from: $DIR/install.sh" >> "$BASHPROFILE_OUT"
-    echo "'source $BASH_IN' -> $BASHPROFILE_OUT"
+    echo "'sh: source $BASH_IN' -> $BASHPROFILE_OUT"
 else
     echo "${UP_TO_DATE_MSG}${BASHPROFILE_OUT}"
 fi
@@ -47,7 +47,7 @@ fi
 BASHRC_OUT="$HOME/.bashrc"
 if ! grep -Fsq "source $BASH_IN" "$BASHRC_OUT"; then
     echo "source $BASH_IN # from: $DIR/install.sh" >> "$BASHRC_OUT"
-    echo "'source $BASH_IN' -> $BASHRC_OUT"
+    echo "'sh: source $BASH_IN' -> $BASHRC_OUT"
 else
     echo "${UP_TO_DATE_MSG}${BASHRC_OUT}"
 fi
